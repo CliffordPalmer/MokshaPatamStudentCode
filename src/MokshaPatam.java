a4358dd63f744467f21d8654540f5143a16d5283
Clifford <clifford.palmer@menloschool.org>
2024-09-03 20:52:04 -0700

    First session

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -7,6 +7,10 @@
  * Completed by: [YOUR NAME HERE]
  *
  */
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class MokshaPatam {
 
@@ -15,7 +19,29 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        Queue<Integer> toSearch = new LinkedList<Integer>();
+        int current = 1;
+
+        while (current != boardsize) {
+            for (int i = current + 1; i <= current + 6; i++) {
+                if (i <= boardsize) {
+                    for(int j = 0; j < ladders.length; j++) {
+                        if (ladders[j][0] == i) {
+                            toSearch.add(ladders[j][1]);
+                        }
+                    }
+                    for(int j = 0; j < snakes.length; j++) {
+                        if (snakes[j][0] == i) {
+                            toSearch.add(snakes[j][1]);
+                        }
+                    }
+                }
+
+            }
+        }
 
         return 0;
     }
+
+
 }
