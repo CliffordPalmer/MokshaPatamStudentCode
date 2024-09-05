bfe33358b069a3c253fe3bece6d09a2c7d58378e
Clifford <clifford.palmer@menloschool.org>
2024-09-05 14:49:56 -0700

    First session

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -23,21 +23,19 @@ public class MokshaPatam {
         Queue<Integer> nextLevel = new LinkedList<Integer>();
         int level = 0;
 
+        int[] info = new int[boardsize + 1];
+        boolean[] visited = new boolean[boardsize + 1];
+        int[] depth = new int[boardsize + 1];
+
         int current = 1;
         toSearch.add(1);
+        depth[1] = 1;
 
-        int[] info = new int[boardsize + 1];
-        boolean[] visited = new boolean[boardsize + 1]
-        int[] depth = new int[boardsize + 1];
-        for(int i = 0; i < boardsize; i++){
-            info[i][0] = -1;
-            info[i][1] = 0;
-        }
         for(int i = 0; i < ladders.length; i++){
-            info[ladders[i][0] - 1][0] = ladders[i][1];
+            info[ladders[i][0]] = ladders[i][1];
         }
         for(int i = 0; i < snakes.length; i++){
-            info[snakes[i][0] - 1][0] = snakes[i][1];
+            info[snakes[i][0]] = snakes[i][1];
         }
 
         while (current != boardsize) {
@@ -52,18 +50,18 @@ public class MokshaPatam {
                 break;
             }
             for (int i = current + 1; i <= current + 5; i++) {
-                if (info[i - 1][0] != -1 && info[i - 1][1] == 0) {
-                    if(info[i - 1][0] == boardsize){
+                if (info[i] != 0 && !visited[i]) {
+                    if(info[i] == boardsize){
                         level ++;
                         return level;
                     }
-                    nextLevel.add(info[i - 1][0]);
-                    info[i - 1][1] = 1;
+                    nextLevel.add(info[i]);
+                    visited[i] = true;
                 }
             }
-            if(info[current + 6][0] != -1 && info[current+ 6][1] == 0){
+            if(info[current + 6] != 0 && visited[current+ 6] == false){
                 nextLevel.add(current + 6);
-                info[current + 6][1] = 1;
+                visited[current + 6] = true;
             }
             else {
                 nextLevel.add(current + 6);
