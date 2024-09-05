d91d2b035b9e8de2d3828ef1f4e2ea1c8f50424e
Clifford <clifford.palmer@menloschool.org>
2024-09-04 23:16:43 -0700

    First session

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -20,27 +20,45 @@ public class MokshaPatam {
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
         Queue<Integer> toSearch = new LinkedList<Integer>();
+        Queue<Integer> nextLevel = new LinkedList<Integer>();
+        int level = 0;
+
         int current = 1;
 
+        int[] sAndL = new int[boardsize];
+        for(int i = 0; i < boardsize; i++){
+            sAndL[i] = -1;
+        }
+        for(int i = 0; i < ladders.length; i++){
+            sAndL[ladders[i][0] - 1] = ladders[i][1];
+        }
+        for(int i = 0; i < snakes.length; i++){
+            sAndL[snakes[i][0] - 1] = snakes[i][1];
+        }
+
         while (current != boardsize) {
-            for (int i = current + 1; i <= current + 5; i++) {
-                if (i <= boardsize) {
-                    for(int j = 0; j < ladders.length; j++) {
-                        if (ladders[j][0] == i) {
-                            toSearch.add(ladders[j][1]);
-                        }
-                    }
-                    for(int j = 0; j < snakes.length; j++) {
-                        if (snakes[j][0] == i) {
-                            toSearch.add(snakes[j][1]);
-                        }
-                    }
+            if(toSearch.isEmpty()){
+                toSearch = nextLevel;
+                nextLevel = new LinkedList<Integer>();
+                level ++;
+            }
+            if(current + 6 < boardsize){
+                break;
+            }
+            for (int i = current + 1; i < current + 5; i++) {
+                if (sAndL[i] != -1) {
+                    toSearch.add(sAndL[i]);
                 }
                 else{
                     break;
                 }
             }
-            toSearch.add(current + 6);
+            if(sAndL[current + 6 - 1] != -1){
+                toSearch.add(sAndL[current + 6]);
+            }
+            else {
+                toSearch.add(current + 6);
+            }
             current = toSearch.remove();
         }
 
